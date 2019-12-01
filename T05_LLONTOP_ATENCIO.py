e1399718792d0909cce4e2953e3dc7ca31d22253
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-11-30 21:53:26 -0500

    Se agrego el ejercicio 03

--- T05_LLONTOP_ATENCIO.py
+++ T05_LLONTOP_ATENCIO.py
@@ -20,3 +20,15 @@ monto_total_de_un_ganadero_es_int=isinstance(monto_total_de_un_ganadero,int)
 print("variable monto total de un ganadero es:",type_monto_total_de_un_ganadero)
 print("variable es float?:",monto_total_de_un_ganadero_es_float)
 print("variable es int?:",monto_total_de_un_ganadero_es_int)
+
+
+#EJERCICIO_03
+ingreso_total_de_una_consultora_natura=557.98
+type_ingreso_total_de_una_consultora_natura=type(ingreso_total_de_una_consultora_natura)
+
+ingreso_total_de_una_consultora_natura_es_float=isinstance(ingreso_total_de_una_consultora_natura,float)
+ingreso_total_de_una_consultora_natura_es_str=isinstance(ingreso_total_de_una_consultora_natura,str)
+
+print("variable ingreso total de una consultora natura es:",type_ingreso_total_de_una_consultora_natura)
+print("variable es float?:",ingreso_total_de_una_consultora_natura_es_float)
+print("variable es str?:",ingreso_total_de_una_consultora_natura_es_str)
